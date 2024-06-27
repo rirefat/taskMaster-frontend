5b6c8da4a2c3190a4175c89f59bb2ed125b9afdb
rirefat <rirefat.official@gmail.com>
2024-06-28 02:38:43 +0600

    solve shadcn issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+    <Button>Click me</Button>
+  </div>
   );
 };
 
