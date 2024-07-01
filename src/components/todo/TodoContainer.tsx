0f3e2d30de95ceff208982cf745672c5aed66436
rirefat <rirefat.official@gmail.com>
2024-07-02 01:50:46 +0600

    connect with server using rtk query

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,11 +1,20 @@
 import EmptyCard from "./EmptyCard";
-import { useAppSelector } from "@/redux/hook";
+// import { useAppSelector } from "@/redux/hook";
 import { AddTodoModal } from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
-    const { todos } = useAppSelector((state) => state.todo);
+    // getting data from local state
+    // const { todos } = useAppSelector((state) => state.todo);
+
+    // getting data from server
+    const { data:todos, isLoading } = useGetTodosQuery(undefined);
+
+    if(isLoading){
+        return <p>Loading...</p>
+    }
 
     return (
         <div>
@@ -17,8 +26,8 @@ const TodoContainer = () => {
             <div className="bg-primary-gradient w-full h-full rounded-md shadow-lg p-[5px]">
                 <div className="bg-white p-5 w-full h-full rounded-md space-y-3 ">
                     {
-                        todos.length > 0 ? todos.map((item) => (
-                            <TodoCard key={item.id} item={item} />
+                        todos?.data?.length > 0 ? todos?.data?.map((item) => (
+                            <TodoCard key={item._id} item={item} />
                         )) : <EmptyCard />
                     }
                 </div>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from './features/todoSlice';
+import { baseApi } from "./api/api";
 
 export const store = configureStore({
     reducer: {
+        [baseApi.reducerPath]: baseApi.reducer,
         todo: todoReducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
